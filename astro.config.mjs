7a881535b6a1999ef3c1ac5644b8b972cc133d82
Cyrus <26298582+dwaynemcyrus@users.noreply.github.com>
2025-12-29 20:46:13 +0100

    Add Tailwind config and static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   site: "https://dwaynemcyrus.com",
   integrations: [sitemap(), mdx(), pagefind()],
   vite: {
